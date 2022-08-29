11c10652f3f69390a8f6aabd8b8e26e67fa6e098
vickynocampoemail <you@example.com>
2022-08-29 17:54:37 -0300

    clase 39

--- modulo-8.js
+++ modulo-8.js
@@ -284,3 +284,96 @@ window.addEventListener('load', function(){
       this.innerText = "este enlace no te llevará a ningún lado"   
   })
 })
+
+// onfocus
+// Estamos buscando darle un poco más de dinamismo a nuestro sitio web y por eso queremos, ahora que tenemos a disposición las herramientas de JavaScript en el front-end, que empiecen a pasar cosas cuando el usuario navega por nuestra página. Por ejemplo, que el fondo del input de un formulario se vuelva rosa al hacer clic en el mismo.
+// Para ello debemos completar las siguentes tareas:
+// Capturar el input con id "nombre" y guardarlo dentro de la variable "elNombre".
+// Asignarle el evento "focus" a la variable que almacena ese input.
+// Cuando el input esta en foco, que el fondo cambie a color rosa.
+// Completemos el siguiente código.
+window.addEventListener('load', function () {
+  // Escribi tu codigo aca :)
+  let elNombre = document.querySelector('#nombre');
+  elNombre.addEventListener('focus', function () {
+      elNombre.style.backgroundColor = "pink";
+  })
+})
+
+// onblur
+// Tenemos un usuario que está tratando de iniciar sesión en nuestro sitio y queremos mejorar su experiencia como usuario
+// Para ello, haremos lo siguiente:
+// Almacenar dentro de una variable con nombre "elUsuario", el input que tiene como id "user".
+// Asignar a esa variable el evento blur para detectar cuando el usuario haga clic fuera del input.
+// Cambiar el color del input a "plum" cuando el usuario ejecute el evento blur.
+// Completemos el siguiente código.
+window.addEventListener('load', function(){
+  let elUsuario = document.querySelector('#user');
+  elUsuario.addEventListener('blur', function(){
+     elUsuario.style.backgroundColor = "plum"
+  })
+})
+
+// onchange
+// Ahora que conocemos más sobre eventos del formulario, queremos que, cuando una persona haga algún cambio dentro de un input, el color del <body> cambie a rojo.
+// Para eso necesitamos capturar el input con id "miInput" dentro de la variable "miInput", asignarle el evento change y cambiarle el color cuando se realice el cambio.
+window.addEventListener('load', function () {
+  // Escribe tu codigo aquí
+  let miInput = document.querySelector('#miInput');
+  miInput.addEventListener('change', function () {
+      document.body.style.backgroundColor = "red";
+  })
+})
+
+// onsubmit
+// Tenemos un documento HTML dentro del cual tenemos un formulario y un mensaje oculto para el usuario. Queremos que cuando el usuario pulse en enviar se oculte el formulario y se muestre el mensaje oculto.
+// Para ello, haremos lo siguiente:
+// Capturar el formulario con id "miForm" en la variable "miForm".
+// Capturar el mensaje dentro de la etiqueta <h1> en la variable "mensajeOculto".
+// Asignar a la variable "miForm" el evento "submit" para detectar cuándo se envía el formulario.
+// Cambiar el display del formulario a "none".
+// Cambiar el display del mensaje a "block".
+let miForm = document.querySelector('#miForm');
+let mensajeOculto = document.querySelector('h1');
+miForm.addEventListener('submit', function (){
+    miForm.style.display = "none";
+    mensajeOculto.style.display = "block";
+})
+
+×
+
+// Prevent default
+// Antes de empezar a validar...
+// Debemos crear la variable formulario y almacenar en ella el formulario del HTML. Una vez capturado, deberemos asignarle el evento "submit" y prevenir el comportamiento por defecto del formulario.
+// Para este ejercicio, teenmos que pasarle al callback del evento la variable con nombre evento.
+// Para este ejercicio, tenemos que pasarle al callback del evento la variable con nombre evento.
+window.addEventListener('load', function () {
+  // escribí tu código aquí
+  let formulario = document.querySelector('form');
+  formulario.addEventListener('submit', function (evento) {
+    evento.preventDefault();
+  })
+});
+
+// Validando campos
+// Validemos que los datos que completa el usuario sean correctos.
+// Para eso debemos:
+// Capturar el input con id nombre en la variable nombre.
+// Si el nombre está vacío, hacer un console.log() con el texto 'Hubo un error en el nombre'.
+// Capturar el input con id password en la variable password.
+// Si el password tiene menos de 4 caracteres, hacer un console.log() con el texto 'Hubo un error en el password'.
+window.addEventListener('load', function () {
+  let formulario = document.querySelector('form');
+  formulario.addEventListener('submit', function (evento) {
+    evento.preventDefault();
+    // escribí tu código aquí
+    let nombre = document.querySelector('#nombre');
+    if (nombre.value == "") {
+      console.log("Hubo un error en el nombre")
+    };
+    let password = document.querySelector('#password');
+    if (password.value.length < 4) {
+      console.log("Hubo un error en el password")
+    }
+  })
+});
